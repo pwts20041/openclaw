2b520bf0baa3174aa65eac5d9a593670e37bef80
chembo.huang <chembo.huang@vipshop.com>
2026-03-20 00:08:47 +0800

    control-ui: improve Mermaid interactions

--- ui/src/ui/views/markdown-sidebar.ts
+++ ui/src/ui/views/markdown-sidebar.ts
@@ -1,5 +1,6 @@
 import { html } from "lit";
 import { unsafeHTML } from "lit/directives/unsafe-html.js";
+import { handleCodeBlockCopyClick } from "../code-block-copy.ts";
 import { icons } from "../icons.ts";
 import { toSanitizedMarkdownHtml } from "../markdown.ts";
 
@@ -17,7 +18,7 @@ export function renderMarkdownSidebar(props: MarkdownSidebarProps) {
         <div class="sidebar-title">Tool Output</div>
         <button @click=${props.onClose} class="btn" title="Close sidebar">${icons.x}</button>
       </div>
-      <div class="sidebar-content">
+      <div class="sidebar-content" @click=${handleCodeBlockCopyClick}>
         ${
           props.error
             ? html`
